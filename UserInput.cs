7eab118ca3a750df2feff919895cce29e4b524db
MKEABB <118346347+MKEABB@users.noreply.github.com>
2023-11-27 09:31:44 +0100

    UserInput metod för snyggare inmatning

--- UserInput.cs
+++ UserInput.cs
@@ -8,5 +8,31 @@ namespace LLL_Grupp_6
 {
     internal class UserInput
     {
+        public static string String(string outMessage, ref string stringVariable)
+        {
+            try
+            {
+                Console.WriteLine(outMessage);
+                stringVariable = (Console.ReadLine().ToUpper());
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            return stringVariable;
+        }
+        public static int Int(string outMessage, ref int intVariable)
+        {
+            try
+            {
+                Console.WriteLine(outMessage);
+                intVariable = int.Parse(Console.ReadLine());
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            return intVariable;
+        }
     }
 }
