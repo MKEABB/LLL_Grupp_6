5ef990c22a9aa1ef1e2fe1e1b06ef0c611bf53ca
MKEABB <118346347+MKEABB@users.noreply.github.com>
2023-11-27 09:29:40 +0100

    UserInput-metod för att snygga till användarinmatning + commit to master test

---
+++ UserInput.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LLL_Grupp_6
+{
+    internal class UserInput
+    {
+    }
+}
