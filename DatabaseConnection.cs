5565d8de99eab28c11b8660bdbf9b160950d105a
Jonathan Gunnerud <131014056+JonoByte@users.noreply.github.com>
2023-11-28 09:27:16 +0100

    Auto create database when starting the program

--- DatabaseConnection.cs
+++ DatabaseConnection.cs
@@ -1,27 +1,76 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Data.SqlClient;
 
 namespace LLL_Grupp_6
 {
     class DatabaseConnection
     {
-        private SqlConnection connection;
+        private SqlConnection connection; // Connection to the database
+        private readonly string connectionString; // Connection string
+        private readonly string dbName = "Ninja-Astronauts-DB"; // Database name
 
-        public DatabaseConnection()
+        public DatabaseConnection() // Constructor for the DatabaseConnection class
         {
-            string connectionString = @"Data Source=.\SQLEXPRESS; Initial Catalog=Ninja-Astronauts-DB; Integrated Security=true; TrustServerCertificate=true;";
+            connectionString = @"Data Source=.\SQLEXPRESS; Integrated Security=true; TrustServerCertificate=true;";
             connection = new SqlConnection(connectionString);
+
+            InitializeDatabase(); // Call this method to ensure the database and tables are set up when the connection is instantiated
         }
 
-        public void OpenConnection()
+        private void InitializeDatabase() // Method for initializing the database and tables
+        {
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+
+                
+                string checkDbQuery = $"SELECT db_id('{dbName}')"; // SQL query to check if the database exists or not (returns NULL if it doesn't exist)
+                using (SqlCommand checkDbCommand = new SqlCommand(checkDbQuery, conn))
+                {
+                    if (checkDbCommand.ExecuteScalar() == DBNull.Value) // If the database doesn't exist, create it
+                    {
+                        // Create database
+                        string createDbQuery = $"CREATE DATABASE {dbName}";
+                        using (SqlCommand createDbCommand = new SqlCommand(createDbQuery, conn))
+                        {
+                            createDbCommand.ExecuteNonQuery();
+                        }
+
+                        // Create tables
+                        conn.ChangeDatabase(dbName); // Change to the newly created database
+                        CreateTables(conn); // Call method for creating the tables
+                    }
+                }
+            }
+        }
+        private void CreateTables(SqlConnection conn) // Method for creating the tables in the database
+        {
+            // SQL command to create Pallet table
+            string createPalletTableQuery = @"CREATE TABLE Pallet (
+                                                  PalletID INT PRIMARY KEY NOT NULL,
+                                                  PalletType NVARCHAR(10) NOT NULL,
+                                                  ArrivalTime DATETIME NOT NULL
+                                              );";
+            SqlCommand createPalletTableCommand = new SqlCommand(createPalletTableQuery, conn);
+            createPalletTableCommand.ExecuteNonQuery();
+
+            // SQL command to create Storage table
+            string createStorageTableQuery = @"CREATE TABLE Storage (
+                                                   StorageID INT PRIMARY KEY NOT NULL,
+                                                   ShelfID1 INT,
+                                                   FOREIGN KEY (ShelfID1) REFERENCES Pallet(PalletID),
+                                                   ShelfID2 INT,
+                                                   FOREIGN KEY (ShelfID2) REFERENCES Pallet(PalletID)
+                                               );";
+            SqlCommand createStorageTableCommand = new SqlCommand(createStorageTableQuery, conn);
+            createStorageTableCommand.ExecuteNonQuery();
+        }
+
+        public void OpenConnection() // Method for opening the connection to the database
         {
             try
             {
-                if (connection.State == System.Data.ConnectionState.Closed)
+                if (connection.State == System.Data.ConnectionState.Closed) // Check if connection is closed before opening it
                 {
                     connection.Open();
                 }
@@ -30,13 +79,12 @@ namespace LLL_Grupp_6
             {
                 Console.WriteLine("Error opening connection: " + e.Message);
             }
-        }   
-
-        public void CloseConnection()
+        }
+        public void CloseConnection() // Method for closing the connection to the database
         {
             try
             {
-                if (connection.State == System.Data.ConnectionState.Open)
+                if (connection.State == System.Data.ConnectionState.Open) // Check if connection is open before closing it
                 {
                     connection.Close();
                 }
@@ -46,8 +94,7 @@ namespace LLL_Grupp_6
                 Console.WriteLine("Error closing connection: " + e.Message);
             }
         }
-
-        public SqlConnection GetConnection()
+        public SqlConnection GetConnection() // Method for getting the connection to the database
         {
             return connection;
         }
