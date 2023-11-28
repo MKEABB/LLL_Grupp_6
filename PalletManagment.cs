3e285afb6097cfc0054ae63498e1fb6a2e0e68b1
MKEABB <118346347+MKEABB@users.noreply.github.com>
2023-11-28 09:20:27 +0100

    PalletManagement, utök av sökmetod

--- PalletManagment.cs
+++ PalletManagment.cs
@@ -6,10 +6,11 @@ using System.Threading.Tasks;
 using System.Data.SqlClient;
 
 namespace LLL_Grupp_6
+
 {
     public class PalletManagment
     {
-        private DatabaseConnection dbConnection;
+        private static DatabaseConnection dbConnection;
 
         public PalletManagment()
         {
@@ -39,12 +40,15 @@ namespace LLL_Grupp_6
                 dbConnection.CloseConnection();
             }
         }
-        public void RetrievePallet(int palletId) // Fetches and displays the information of a specific pallet based on its ID.
+        public static Tuple<int, string, DateTime, int> RetrievePallet(int palletId)                    // Fetches and displays the information of a specific pallet based on its ID.
         {
+            Tuple <int, string, DateTime, int> palletTuple = null;
             try
             {
                 dbConnection.OpenConnection();
-                string query = "SELECT * FROM Pallet WHERE PalletID = @PalletID";
+                string query = "SELECT p.*, s.StorageID FROM Pallet p\r\n" +
+                               "JOIN Storage s ON s.ShelfID1 = p.PalletID OR s.ShelfID2 = p.PalletID\r\n" +
+                               "WHERE p.PalletID = @PalletID;";
 
                 using (SqlCommand command = new SqlCommand(query, dbConnection.GetConnection()))
                 {
@@ -57,10 +61,20 @@ namespace LLL_Grupp_6
                             Console.WriteLine("Pallet ID: " + reader["PalletID"]);
                             Console.WriteLine("Pallet Type: " + reader["PalletType"]);
                             Console.WriteLine("Arrival Time: " + reader["ArrivalTime"]);
+                            Console.WriteLine("StorageID: " + reader["StorageID"]);
+
+                            int tPalletID = Convert.ToInt32(reader["PalletID"]);
+                            string tPalletType = reader["PalletType"].ToString();
+                            DateTime tDateTime = reader.GetDateTime(reader.GetOrdinal("ArrivalTime"));
+                            int tStorageID = Convert.ToInt32(reader["StorageID"]);
+
+                            palletTuple = new Tuple<int, string, DateTime, int>(
+                                                    tPalletID, tPalletType, tDateTime, tStorageID);
                         }
                         else
                         {
-                            Console.WriteLine("No pallet with that ID exists." + palletId);
+                            Console.WriteLine("No pallet with ID {0} exists.", palletId);
+                            return null;
                         }
                     }
                 }
@@ -73,6 +87,7 @@ namespace LLL_Grupp_6
             {
                 dbConnection.CloseConnection();
             }
+            return palletTuple;
         }
 
         public void DeletePallet(int palletId) // Deletes a pallet based on its ID.
@@ -85,7 +100,7 @@ namespace LLL_Grupp_6
                 using (SqlCommand command = new SqlCommand(query, dbConnection.GetConnection()))
                 {
                     command.Parameters.AddWithValue("@PalletID", palletId);
-                    
+
                     int rowsAffected = command.ExecuteNonQuery();
                     if (rowsAffected > 0)
                     {
@@ -105,6 +120,7 @@ namespace LLL_Grupp_6
             {
                 dbConnection.CloseConnection();
             }
+
         }
     }
 }
