68961c602d35d26e116235c6799eb24e43dcc373
Jonathan Gunnerud <131014056+JonoByte@users.noreply.github.com>
2023-11-28 09:54:03 +0100

    Removed some unused code and pasted mikeala's retrievepallet code

--- PalletManagment.cs
+++ PalletManagment.cs
@@ -16,42 +16,15 @@ namespace LLL_Grupp_6
         {
             dbConnection = new DatabaseConnection();
         }
-
-        //public void AddPallet(int palletId,string palletType) // Adds a pallet to the database with the specified ID and type.
-        //{
-        //    try
-        //    {
-
-
-        //        dbConnection.OpenConnection();
-
-        //        int storageID = AvailableStorage();
-
-        //        string query = "INSERT INTO Pallet (PalletType, ArrivalTime) VALUES (@PalletType, GETDATE())";
-
-        //        using (var command = new SqlCommand(query, dbConnection.GetConnection()))
-        //        {
-        //            command.Parameters.AddWithValue("@PalletID", palletId);
-        //            command.Parameters.AddWithValue("@PalletType", palletType);
-        //            command.ExecuteNonQuery();
-        //        }
-                
-        //    }
-        //    catch (SqlException e)
-        //    {
-        //        Console.WriteLine("Error adding pallet: " + e.Message);
-        //    }
-        //    finally
-        //    {
-        //        dbConnection.CloseConnection();
-        //    }
-        //}
-        public void RetrievePallet(int palletId) // Fetches and displays the information of a specific pallet based on its ID.
+        public Tuple<int, string, DateTime, int> RetrievePallet(int palletId)  // Fetches and displays the information of a specific pallet based on its ID.
         {
+            Tuple<int, string, DateTime, int> palletTuple = null;
             try
             {
                 dbConnection.OpenConnection();
-                string query = "SELECT * FROM Pallet WHERE PalletID = @PalletID";
+                string query = "SELECT p.*, s.StorageID FROM Pallet p\r\n" +
+                               "JOIN Storage s ON s.ShelfID1 = p.PalletID OR s.ShelfID2 = p.PalletID\r\n" +
+                               "WHERE p.PalletID = @PalletID;";
 
                 using (SqlCommand command = new SqlCommand(query, dbConnection.GetConnection()))
                 {
@@ -64,10 +37,20 @@ namespace LLL_Grupp_6
                             Console.WriteLine("Pallet ID: " + reader["PalletID"]);
                             Console.WriteLine("Pallet Type: " + reader["PalletType"]);
                             Console.WriteLine("Arrival Time: " + reader["ArrivalTime"]);
+                            Console.WriteLine("StorageID: " + reader["StorageID"]);
+
+                            int tPalletID = Convert.ToInt32(reader["PalletID"]);
+                            string tPalletType = reader["PalletType"].ToString();
+                            DateTime tDateTime = reader.GetDateTime(reader.GetOrdinal("ArrivalTime"));
+                            int tStorageID = Convert.ToInt32(reader["StorageID"]);
+
+                            palletTuple = new Tuple<int, string, DateTime, int>(
+                                                    tPalletID, tPalletType, tDateTime, tStorageID);
                         }
                         else
                         {
-                            Console.WriteLine("No pallet with that ID exists." + palletId);
+                            Console.WriteLine("No pallet with ID {0} exists.", palletId);
+                            return null;
                         }
                     }
                 }
@@ -80,46 +63,7 @@ namespace LLL_Grupp_6
             {
                 dbConnection.CloseConnection();
             }
-        }
-
-        public void DeletePallet(int palletId) // Deletes a pallet based on its ID.
-        {
-            try
-            {
-                dbConnection.OpenConnection();
-
-               
-
-
-                string query = "DELETE FROM Pallet WHERE PalletID = @PalletID";
-
-                using (SqlCommand command = new SqlCommand(query, dbConnection.GetConnection()))
-                {
-                    command.Parameters.AddWithValue("@PalletID", palletId);
-                    
-                    int rowsAffected = command.ExecuteNonQuery();
-                    if (rowsAffected > 0)
-                    {
-                        Console.WriteLine("Pallet deleted successfully.");
-                    }
-                    else if (rowsAffected == 0)
-                    {
-                        Console.WriteLine("No pallet with that ID exists: " + palletId);
-                    }
-                }
-            }
-            catch (SqlException e)
-            {
-                Console.WriteLine("Error deleting pallet: " + e.Message);
-            }
-            finally
-            {
-                dbConnection.CloseConnection();
-            }
-        }
-
-        
-
-        
+            return palletTuple;
+        } 
     }
 }
